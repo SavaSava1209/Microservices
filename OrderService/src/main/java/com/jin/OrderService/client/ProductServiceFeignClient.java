b21dfe83b518a2f55395ec80b74ca00528468430
Chieh-Yin Wang <chieh-yinwang@Chieh-Yins-MacBook-Pro.local>
2023-10-09 12:09:14 -0700

    intro feign client to order service to connect to product reduce quantity api

--- OrderService/src/main/java/com/jin/OrderService/client/ProductServiceFeignClient.java
+++ OrderService/src/main/java/com/jin/OrderService/client/ProductServiceFeignClient.java
@@ -1,2 +1,14 @@
-package com.jin.OrderService.client;public class ProductServiceFeignClient {
+package com.jin.OrderService.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="PRODUCT-SERVICE/product")
+public interface ProductServiceFeignClient {
+    //將 product service 裡的 api dependency 注入進來
+    @PutMapping("/reduceQuantity")
+    public ResponseEntity<Void> reduceProductQuantity(@RequestParam long id, @RequestParam long quantity);
 }
